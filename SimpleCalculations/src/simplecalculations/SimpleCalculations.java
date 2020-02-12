befb4548fbd334573953e07ed6e9d88d7d2898ae
ZaineWoolf <60592490+ZaineWoolf@users.noreply.github.com>
2020-02-12 16:51:33 +1300

    Add files via upload

---
+++ SimpleCalculations/src/simplecalculations/SimpleCalculations.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simplecalculations;
+
+/**
+ *
+ * @author 92060054
+ */
+import java.util.Scanner;
+
+public class SimpleCalculations {
+    
+    
+                
+    
+    public static void main(String[] args) {
+       
+        System.out.println("Please enter the first floating number");
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println("Please enter the second floating number");
+        
+        input = sc.nextInt();
+           
+        float num1 = Float.valueOf(input);
+    
+        System.out.println(calculate);
+        
+        
+        }
+    
+
+    }
+    
+
